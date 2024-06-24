6e51d625125e67ee73fa47224874ecb3c8fb9402
Malvi Medui <129746042+Malvi1697@users.noreply.github.com>
2024-06-24 14:06:33 +0200

    Hello world msg

---
+++ mysite/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from visits.models import PageVisit
+
+
+def home_page_view(request):
+    PageVisit.objects.create(path=request.path)
+    qs_total = PageVisit.objects.all()
+    qs_count = PageVisit.objects.filter(path=request.path)
+    my_page = 'Vseticek.com'
+
+    context = {
+        'page_title': my_page,
+        'queryset': qs_count,
+        'page_visits_total': qs_total.count(),
+        'page_visits': qs_count.count(),
+    }
+    return render(request, 'home.html', context)
